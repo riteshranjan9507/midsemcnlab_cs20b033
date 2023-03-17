42873a0adb322ad4b51ca3b855dc8212a9c32990
Ritesh Ranjan <110907544+riteshranjan9507@users.noreply.github.com>
2023-03-17 10:47:44 +0530

    cs20b033: all random variable created

--- midsemcnlab_cs20b033.py
+++ midsemcnlab_cs20b033.py
@@ -23,3 +23,28 @@ def analysis(num_devices, p):
     expected_slots = np.sum(inv[0])
     
     return expected_slots
+def simulation(num_devices, p):
+    # Transition probability matrix
+    P = np.zeros((num_devices+1, num_devices+1))
+    for i in range(num_devices+1):
+        for j in range(num_devices+1):
+            if i == j:
+                if i == 0:
+                    P[i][j] = 1
+                else:
+                    P[i][j] = 1 - p
+            elif j == i-1:
+                P[i][j] = p
+    
+    # Generate 1000 random initial states
+    initial_states = np.random.randint(1, num_devices+1, size=1000)
+    
+    # Simulate the Markov chain for each initial state
+    num_slots = []
+    for init_state in initial_states:
+        curr_state = init_state
+        num_steps = 0
+        while curr_state != 0:
+            curr_state = np.random.choice(range(num_devices+1), p=P[curr_state])
+            num_steps += 1
+        num_slots.append(num_steps)
